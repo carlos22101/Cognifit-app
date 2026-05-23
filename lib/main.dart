7ad17cedd9201ae31a3039122f24dadd2059b0a9
carlos22101 <233429@ids.upchiapas.edu.mx>
2026-05-23 01:13:17 -0600

    Flag screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _setSecureFlag();
   runApp(const ProviderScope(child: CogniFitApp()));
+}
+
+Future<void> _setSecureFlag() async {
+  const channel = MethodChannel('flutter/platform');
+  try {
+    await channel.invokeMethod('SystemNavigator.setSecureFlag', true);
+  } catch (_) {}
 }
 
 class CogniFitApp extends StatelessWidget {
